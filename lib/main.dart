398b859b9044edd28183be8fd13640c5a5179be5
Iwo Ricahrd <richardiwo1@gmail.com>
2023-03-11 01:06:12 +0100

    url strategy

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:biocard/routes/app_routes.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() async {
+  setPathUrlStrategy();
   WidgetsFlutterBinding.ensureInitialized();
   if(kIsWeb){
     await Firebase.initializeApp(
